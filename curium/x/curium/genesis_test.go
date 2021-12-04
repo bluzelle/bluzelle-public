b9ea7b6c5ca227b6e3d0d84ba64c91d2bdb71659
scott <scott@bulldoginfo.com>
2021-12-04 00:11:40 -0700

    adding curium back in

---
+++ curium/x/curium/genesis_test.go
@@ -0,0 +1,23 @@
+package curium_test
+
+import (
+	"testing"
+
+	keepertest "github.com/bluzelle/curium/testutil/keeper"
+	"github.com/bluzelle/curium/x/curium"
+	"github.com/bluzelle/curium/x/curium/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.CuriumKeeper(t)
+	curium.InitGenesis(ctx, *k, genesisState)
+	got := curium.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
