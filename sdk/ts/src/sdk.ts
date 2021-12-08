5fd67e71dacae8d95ca6bf621d24b3851e6a14fc
scott <scott@bulldoginfo.com>
2021-12-08 03:05:30 -0700

    updated config.yml

--- sdk/ts/src/sdk.ts
+++ sdk/ts/src/sdk.ts
@@ -1,33 +1,54 @@
 import {DirectSecp256k1HdWallet} from "@cosmjs/proto-signing";
-import {txClient} from "./generated/bluzelle/curium/bluzelle.curium.storage/module";
+import {queryClient, txClient} from "./generated/bluzelle/curium/bluzelle.curium.storage/module";
 import {Some} from 'monet'
 
 export interface BluzelleConfig {
-    url: string,
+    url: string
     mnemonic: string
 }
 
-export interface BluzelleSdk {
+export interface BluzelleWallet {
+    hdWallet: DirectSecp256k1HdWallet;
+    address: string;
+    config: BluzelleConfig;
 }
 
-export const bluzelle = (config: BluzelleConfig) =>
+export const bluzelleWallet = (config: BluzelleConfig): Promise<BluzelleWallet> =>
     DirectSecp256k1HdWallet.fromMnemonic(config.mnemonic, {prefix: 'bluzelle'})
-        .then(wallet => Promise.all([
-            wallet.getAccounts(),
-            txClient(wallet, {addr: config.url}),
-            () => wallet
-        ]))
-        .then(([accounts, client, wallet]) => ({
-            address: accounts[0].address,
-            wallet, storage: client
-        }))
+        .then(hdWallet =>
+            hdWallet.getAccounts()
+                .then(accounts => ({
+                    hdWallet,
+                    address: accounts[0].address,
+                    config
+                }))
+        )
 
-bluzelle({
+export const storageClient = (wallet: BluzelleWallet) =>
+    Promise.all([
+        txClient(wallet.hdWallet, {addr: wallet.config.url}),
+        queryClient({addr: wallet.config.url})
+    ])
+        .then(([tx, q]) =>
+            ({
+                wallet,
+                tx,
+                q
+            })
+        );
+
+
+const client = bluzelleWallet({
     url: 'localhost:26657',
-    mnemonic: 'key merit cross zoo rally eternal stuff chapter match rally eye cement critic old shell renew staff swim trend black slam adjust frost spot'
-}).then(client =>
-    Some(client)
-        .map(client => [client.storage.msgPin({cid: 'QmQiDiArzCFcMYZKkePM4gTfZZsL22GfDQwf9vjryhGUDB', creator: client.address})])
-        .map(client.storage.signAndBroadcast)
-        .join()
-).then(x => x)
+    mnemonic: 'key merit cross zoo rally eternal stuff chapter match rally eye cement critic old shell renew staff swim trend black slam adjust frost spot',
+}).then(storageClient)
+    .then(client => Promise.all([
+            client.tx.signAndBroadcast([
+                client.tx.msgPin({cid: 'QmQiDiArzCFcMYZKkePM4gTfZZsL22GfDQwf9vjryhGUDB', creator: client.wallet.address}),
+                client.tx.msgPin({cid: 'QmQiDiArzCFcMYZKkePM4gTfZZsL22GfDQwf9vjryhGUDB', creator: client.wallet.address})
+            ]),
+        ])
+    )
+    .then(x => x)
+
+
