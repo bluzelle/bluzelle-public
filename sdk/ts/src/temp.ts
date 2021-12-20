a6b4d24a747d69b978f62b899a821d2968b1be05
Brady Lee <bjnxlee@gmail.com>
2021-12-20 11:45:14 -0500

    updateToml rpc laddr

--- sdk/ts/src/temp.ts
+++ sdk/ts/src/temp.ts
@@ -4,18 +4,18 @@ import {newLocalWallet} from "./wallets/localWallet";
 
 
 newBluzelleClient({
-    url: 'localhost:26657',
+    url: 'localhost:26667',
     wallet: newLocalWallet('prevent desert dawn vast debris discover accident crumble case design craft morning eight produce liar diesel enact jump drop vast curtain parrot option depart')
 })
     .then(client => Promise.all([
         pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
-        pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
-        pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
-        pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
-        pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
-        pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
-        pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
-        pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
+        // pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
+        // pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
+        // pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
+        // pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
+        // pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
+        // pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
+        // pinCid(client, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', {gasPrice: 0, maxGas: 200000}),
     ]))
     .then(x => console.log('good', x))
     .catch(e => console.log('evil', e))
