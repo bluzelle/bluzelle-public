61b4a9f96f7da6e17c2c5b14d575b4962f7c452d
Brady Lee <bjnxlee@gmail.com>
2022-01-17 10:09:10 -0500

    sdk hasContent() and added functional tests
    added default configs to restart ipfs and swarm
    
    performance tests on content generation algorithms
    
    Fix query test and functional test
    
    Increase timeout for CI test
    
    separate functional testing from load testing, replace ws with http
    
    Update memoize getBlzClient
    
    added dependency
    
    code review

---
+++ sdk/ts/src/query.spec.ts
@@ -0,0 +1,56 @@
+import {getBlzClient, restartIpfsServerAndSwarm} from "@bluzelle/testing/src/commonUtils";
+import {generateContent} from "@bluzelle/testing/src/fileUtils";
+import {hasContent} from "./query";
+import {pinCid} from "./tx";
+import {BehaviorSubject} from "rxjs";
+import {times} from "lodash";
+import {passThroughAwait} from "promise-passthrough";
+import {createCtx, withCtxAwait} from "with-context";
+import {create} from "ipfs-http-client";
+import {expect} from "chai";
+
+const ipfsClient = create({host: '127.0.0.1', port: 5001, protocol: 'http'})
+
+const curiumUrl = 'http://localhost:26667';
+const mnemonic = new BehaviorSubject<string>("");
+
+describe('query', function () {
+    this.timeout(120_000);
+
+    beforeEach(() =>
+        restartIpfsServerAndSwarm()
+            .then((swarmMnemonic) => mnemonic.next(swarmMnemonic))
+    )
+
+    it('hasContent should return true if content is pinned', () =>
+        Promise.all(times(2).map(() =>
+            Promise.resolve(generateContent(0.01))
+                .then((content) => ipfsClient.add(content))
+        ))
+            .then((addResults) => createCtx('addResults', () => addResults))
+            .then(withCtxAwait('client', () => getBlzClient(curiumUrl, mnemonic.getValue())))
+            .then(passThroughAwait((ctx) =>
+                Promise.all(ctx.addResults.map((addResult) =>
+                    pinCid(ctx.client, addResult.path, {maxGas: 200000, gasPrice: 0.002})
+                ))
+            ))
+            .then(ctx => Promise.all(ctx.addResults.map(addResult =>
+                hasContent(ctx.client, `${curiumUrl}`, addResult.path)
+                    .then((confirm) => expect(confirm).to.be.true)
+            )))
+    )
+
+    it('hasContent should return false if content is NOT pinned', () =>
+        Promise.all(times(2).map(() =>
+            Promise.resolve(generateContent(0.01))
+                .then((content) => ipfsClient.add(content))
+        ))
+            .then((addResults) => createCtx('addResults', () => addResults))
+            .then(withCtxAwait('client', () => getBlzClient(curiumUrl, mnemonic.getValue())))
+            .then(ctx => Promise.all(ctx.addResults.map(addResult =>
+                hasContent(ctx.client, `${curiumUrl}`, addResult.path)
+                    .then((confirm) => expect(confirm).to.be.false)
+            )))
+    )
+
+})
--- sdk/ts/src/query.ts
+++ sdk/ts/src/query.ts
@@ -1,38 +1,19 @@
-import {BluzelleClient, newBluzelleClient} from "./sdk";
+import {BluzelleClient} from "./sdk";
 import {
     QueryClientImpl,
 } from "./generated/bluzelle/curium/bluzelle.curium.storage/module/types/storage/query";
 import {Tendermint34Client} from "@cosmjs/tendermint-rpc";
 import {createProtobufRpcClient, QueryClient} from "@cosmjs/stargate";
-import {pinCid} from "./tx";
-import {newLocalWallet} from "./wallets/localWallet";
-import {memoize, times} from "lodash";
-import {passThroughAwait} from "promise-passthrough";
-import {withCtxAwait} from "with-context";
-import {generateContentSlow} from "@bluzelle/testing/src/fileUtils";
 import waitUntil from "async-wait-until";
-import {create} from 'ipfs-http-client';
 
-const ipfsClient = create({host: '127.0.0.1', port: 5009, protocol: 'http'})
+export const waitForContent = (client: BluzelleClient, curiumUrl: string, path: string, waitTime: number = 5000) =>
+    waitUntil(
+        () => hasContent(client, curiumUrl, path),
+        { timeout: waitTime },
+    );
 
-const curiumUrl = 'localhost:26657';
-const mnemonic = 'media betray inquiry marble wish hurry scrap quick submit section ozone suffer rhythm crack medal inflict birth cable engine hotel dose fan globe smile';
-
-// times(20).map(() =>
-//     Promise.resolve({content: generateContentSlow(0.01)})
-//         .then(withCtxAwait('addResult', ctx => ipfsClient.add(ctx.content)))
-//         .then(withCtxAwait(('client'), () => getBlzClient(curiumUrl, mnemonic)))
-//         .then(passThroughAwait((ctx) => pinCid(ctx.client, ctx.addResult.path, {maxGas: 200000, gasPrice: 0.002})))
-//         .then((ctx) => waitForContent(ctx.client, ctx.addResult.path))
-//         .then((a) => console.log(a))
-// )
-
-export const waitForContent = (client: BluzelleClient, path: string) =>
-    waitUntil(() => hasContent(client, path));
-
-
-export const hasContent = (client: BluzelleClient, cid: string) =>
-    getRpcClient('http://localhost:26657')
+export const hasContent = (client: BluzelleClient, curiumUrl: string, cid: string) =>
+    getRpcClient(curiumUrl)
         .then(queryClient => queryClient.HasContent({cid}))
         .then(x => x.hasContent);
 
@@ -41,11 +22,3 @@ const getRpcClient = (url: string): Promise<QueryClientImpl> =>
         .then(tendermintClient => new QueryClient(tendermintClient))
         .then(createProtobufRpcClient)
         .then(rpcClient => new QueryClientImpl(rpcClient));
-
-
-// const getBlzClient = memoize((curiumUrl: string, mnemonic: string) =>
-//     newBluzelleClient({
-//         url: curiumUrl,
-//         wallet: newLocalWallet(mnemonic)
-//     })
-// )
