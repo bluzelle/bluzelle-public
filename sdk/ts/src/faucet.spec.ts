2d503bababbf5750bb396f90383c30b8432d71d2
scott <scott@bulldoginfo.com>
2022-01-27 10:34:49 -0700

    curium-utils
    again
    
    create-validator
    
    apphash
    
    again
    
    pre-bundle
    
    sdk
    
    again
    
    pre stargate rebase
    
    rebase try 2
    
    pre starport rebase
    
    sdk
    
    before working with scott
    
    bluzelle faucet testnet true
    
    minting code tests working
    
    replaced private with public repo

--- sdk/ts/src/faucet.spec.ts
+++ sdk/ts/src/faucet.spec.ts
@@ -1,13 +1,37 @@
 import {newBluzelleClient} from "./sdk";
 import {newLocalWallet} from "./wallets/localWallet";
+import {startSwarmWithClient} from "@bluzelle/testing/src/swarmUtils";
+import {defaultSwarmConfig} from "@bluzelle/testing/src/defaultConfigs";
+import {expect} from "chai";
+import {createAddress, mint} from "./faucet";
 
 describe('faucet', () => {
-    it('should mint tokens', () =>
-        newBluzelleClient({
-            wallet: newLocalWallet('drastic rule silk shaft web length patient toss topple series problem file record floor once best review require volcano hip lyrics afford enough budget'),
-            url: 'localhost:26657'
-        }).then(client => {
-            client.queryClient.faucet.Mint({address: 'bluzelle1qst08g0f6hyr7z6a7xpgye3nv4ngtnxzz457zd'})
-        })
-    )
+
+    it('should create an address', () => {
+        return Promise.resolve(createAddress())
+            .then(result => {
+                expect(result.address).to.match(/^bluzelle/)
+                expect(result.address).to.have.length(47)
+                expect(result.mnemonic.split(' ')).to.have.length(24)
+            })
+    })
+
+    it('should mint tokens to a given address', () => {
+        return startSwarmWithClient({...defaultSwarmConfig, bluzelleFaucet: true})
+            .then(({bzSdk}) => mint(bzSdk, 'bluzelle1qst08g0f6hyr7z6a7xpgye3nv4ngtnxzz457zd'))
+            .then(response => {
+                expect(response.mnemonic).to.be.empty
+                expect(response.address).to.equal('bluzelle1qst08g0f6hyr7z6a7xpgye3nv4ngtnxzz457zd')
+            })
+    })
+
+    it('should mint tokens with no given address', () => {
+        return startSwarmWithClient({...defaultSwarmConfig, bluzelleFaucet: true})
+            .then(({bzSdk}) => mint(bzSdk))
+            .then(response => {
+                expect(response.mnemonic.split(' ')).to.have.length(24)
+                expect(response.address).to.match(/^bluzelle/)
+                expect(response.address).to.have.length(47)
+            })
+    })
 })
---
+++ sdk/ts/src/faucet.ts
@@ -0,0 +1,58 @@
+import {BluzelleClient} from "./sdk";
+import {passThroughAwait} from "promise-passthrough";
+import * as bip39 from 'bip39';
+import {Some} from "monet";
+import BIP32Factory from 'bip32';
+import * as ecc from 'tiny-secp256k1';
+import {BIP32Interface} from 'bip32'
+
+const bip32 = BIP32Factory(ecc);
+import {bech32} from "bech32"
+import {
+    QueryBalanceResponse
+} from "./generated/cosmos/cosmos-sdk/cosmos.bank.v1beta1/module/types/cosmos/bank/v1beta1/query";
+import * as delay from "delay";
+import {getAccountBalance} from "./query";
+
+const hdPath = "m/44'/483'/0'/0'";
+const bech32Prefix = "bluzelle"
+
+
+
+export function createAddress(): Promise<{mnemonic: string, address: string}> {
+    return Some({mnemonic: bip39.generateMnemonic(256)})
+        .map(ctx => [ctx, bip39.mnemonicToSeedSync(ctx.mnemonic)])
+        .map(([ctx, seed]) => [ctx, bip32.fromSeed(seed as Buffer)])
+        .map(([ctx, node]) => [ctx, (node as BIP32Interface).derivePath(hdPath)])
+        .map(([ctx, child]) => [ctx, bech32.toWords((child as BIP32Interface).identifier)])
+        .map(([ctx, words]) => [ctx, bech32.encode(bech32Prefix, words as Buffer)])
+        .map(([ctx, address]) => ({...ctx as {mnemonic: string}, address}))
+        .join()
+}
+
+export function mint(client: BluzelleClient, address?: string) {
+    return address ? mintToAddress(address) : mintToNewAddress();
+
+    function mintToAddress(address: string) {
+        return client.queryClient.faucet.Mint({address: address})
+            .then(() => waitUntilFunded(client, address))
+            .then(() => ({mnemonic: "", address}))
+    }
+
+    function mintToNewAddress() {
+        return Promise.resolve(createAddress())
+            .then(passThroughAwait(ctx => client.queryClient.faucet.Mint({address: ctx.address})))
+            .then(passThroughAwait(ctx => waitUntilFunded(client, ctx.address)))
+    }
+}
+
+export function waitUntilFunded(client: BluzelleClient, address: string): Promise<unknown> {
+    return getAccountBalance(client, address)
+        .then(waitForMint);
+
+        function waitForMint(startBalance: number): Promise<unknown> {
+            return getAccountBalance(client, address)
+                .then(passThroughAwait(balance => console.log('waiting for funds...', balance)))
+                .then(balance => balance === startBalance && delay(1000).then(() => waitForMint(startBalance)))
+        }
+}
