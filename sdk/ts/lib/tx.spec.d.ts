1b29981929d220cc9f1b8b264827c876fe1efec9
Scott Burch <scott@bulldoginfo.com>
2022-06-03 08:59:44 -0600

    fixing export tests
    fixing export tests
    
    narrowing down a test
    
    take test network down if it is not proper
    
    message change
    
    small changes
    
    remove .only on one of the tests

--- sdk/ts/lib/tx.spec.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=tx.spec.d.ts.map
--- sdk/ts/lib/utils/Deferred.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Deferred<T> {
-    resolve: (v: T | PromiseLike<T>) => void;
-    reject: <E>(e: E | PromiseLike<E>) => void;
-    promise: Promise<T>;
-}
-export declare const newDeferred: <T>() => Deferred<T>;
-//# sourceMappingURL=Deferred.d.ts.map
