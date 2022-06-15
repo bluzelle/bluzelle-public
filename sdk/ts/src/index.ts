da29e64271f0da02e6f30c1ecd7fb72912e3ab49
Brady Lee <bjnxlee@gmail.com>
2022-06-15 09:34:02 -0400

    token conversion service
    check conversion amount
    
    get total amount of conversion including fees
    
    resolved jest bug using ganache
    
    clean up imports in staking wallet with interface in @bluzelle/testing
    
    send erc20 blz to house wallet
    
    send erc20 blz to house wallet
    
    small clean up

--- sdk/ts/src/index.ts
+++ sdk/ts/src/index.ts
@@ -6,7 +6,7 @@ export {newKeplrWallet} from './wallets/keplrWallet'
 export {mint} from "./faucet"
 export {newBluzelleClient} from "./sdk"
 export type {BluzelleClient} from "./sdk"
-export {getAccountBalance, hasContent} from "./query"
+export {getAccountBalance, hasContent, getTaxInfo} from "./query"
 export {getStatus, getValidators} from "./queryTendermint"
 export {send, pinCid} from "./tx"
 
