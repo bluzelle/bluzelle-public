97f0f39a80364867f4f051edf6573c26b59b9d42
Aven Dauz <avendauz@gmail.com>
2022-08-04 10:46:53 -0400

    protoc fix, useOptionals=messages instead of true

--- sdk/ts/proto-builder/compileProto.ts
+++ sdk/ts/proto-builder/compileProto.ts
@@ -8,7 +8,7 @@ export const ensureDirExists = (dir: string) =>
     mkdir(dir, {recursive: true});
 
 export const generateProto = (protoFilePath: string, toDir: string) =>
-    exec`protoc --plugin=${__dirname}/../node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=${toDir} ${protoFilePath} --proto_path proto/proto --proto_path proto/third_party/proto --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=true"`.toPromise();
+    exec`protoc --plugin=${__dirname}/../node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=${toDir} ${protoFilePath} --proto_path proto/proto --proto_path proto/third_party/proto --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=messages"`.toPromise();
 
 
 export const getProtoPathOption = (filePath: string) =>
