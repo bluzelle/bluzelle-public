f787165037a69f93fb6b38bfb8b9c2152af2e10b
Scott Burch <scott@bulldoginfo.com>
2022-08-05 12:04:54 -0600

    initial browser sdk test project
    set up app
    
    basic transaction working
    
    added raw html sdk test
    
    sdk build script compiles to lib with wasm and type defs

--- sdk/ts/browser.webpack.config.js
+++ sdk/ts/browser.webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.ts',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'lib-web'),
+        path: path.resolve(__dirname, 'lib'),
          library: "bluzelle",
          libraryTarget: "umd",
     },
