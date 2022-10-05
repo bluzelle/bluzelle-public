4c96e2c0221d259d6d2b36c6efe1e58a73f20b72
Aven Dauz <avendauz@gmail.com>
2022-10-05 16:49:38 -0400

    renamed getTx to get-tx, added getTx spec

--- sdk/ts/src/tx.spec.ts
+++ sdk/ts/src/tx.spec.ts
@@ -1,13 +1,15 @@
 import {pinCid, send, setGasTaxBp, setTaxCollector, setTransferTaxBp, withTransaction} from "./tx";
 import {startSwarmWithClient} from "@bluzelle/testing"
 import {defaultSwarmConfig} from "@bluzelle/testing/src/defaultConfigs";
-import {getAccountBalance, getTaxInfo} from "./query";
+import {getAccountBalance, getTaxInfo, getTx} from "./query";
 import {passThroughAwait} from "promise-passthrough";
 import {withCtxAwait} from "@scottburch/with-context";
 import {mint} from "./faucet";
 import {expect} from "chai";
 import {Swarm} from "daemon-manager";
 import {newBluzelleClient} from "./sdk";
+import {newLocalWallet} from "./wallets/localWallet";
+import {generateMnemonic} from "./generateMnemonic";
 
 const MAX_GAS = 200000;
 const GAS_PRICE = 2;
@@ -63,8 +65,14 @@ describe('sending transactions', function () {
                 getAccountBalance(ctx.bzSdk, ctx.toAddress, 'uelt'),
                 getAccountBalance(ctx.bzSdk, ctx.toAddress, 'ug4'),
             ])))
-            .then(passThroughAwait(ctx => send(ctx.bzSdk, ctx.toAddress, 10000, {gasPrice: 0.002, maxGas: 200000}, 'uelt')))
-            .then(passThroughAwait(ctx => send(ctx.bzSdk, ctx.toAddress, 10000, {gasPrice: 0.002, maxGas: 200000}, 'ug4')))
+            .then(passThroughAwait(ctx => send(ctx.bzSdk, ctx.toAddress, 10000, {
+                gasPrice: 0.002,
+                maxGas: 200000
+            }, 'uelt')))
+            .then(passThroughAwait(ctx => send(ctx.bzSdk, ctx.toAddress, 10000, {
+                gasPrice: 0.002,
+                maxGas: 200000
+            }, 'ug4')))
             .then(withCtxAwait('postBalances', ctx => Promise.all([
                 getAccountBalance(ctx.bzSdk, ctx.bzSdk.address, 'uelt'),
                 getAccountBalance(ctx.bzSdk, ctx.bzSdk.address, 'ug4'),
@@ -75,6 +83,33 @@ describe('sending transactions', function () {
             ])))
             .then(passThroughAwait(ctx => expect(ctx.postBalances).to.deep.equal(ctx.preBalances.map(b => b - 10000))))
             .then(ctx => expect(ctx.toPostBalances).to.deep.equal(ctx.toPreBalances.map(b => b + 10000)))
+    );
+
+    it('should return a valid transaction for a failing message', () =>
+        startSwarmWithClient()
+            .then(() => newBluzelleClient({
+                url: 'http://localhost:26667',
+                wallet: newLocalWallet(generateMnemonic())
+            }))
+            .then(passThroughAwait(bzSdk =>
+                mint(bzSdk, bzSdk.address)))
+            .then(bzSdk =>
+                (send(bzSdk, 'bluzelle10dj35urh78dym3c24yzdmss8mxtcy8hqgqwmrn', 300000000, {
+                    gasPrice: 0,
+                    maxGas: 1000000000
+                }, 'bogus') as any)
+                    .then((resp: any) => ({
+                        resp,
+                        bzSdk
+                    })))
+            .then(({bzSdk, resp}) => getTx(bzSdk, resp.transactionHash) as any)
+            .then((response: any) => {
+                expect(response.tx).not.to.be.undefined;
+                expect(response.txResponse).not.to.be.undefined;
+                expect(response.tx.body.messages[0].typeUrl).to.deep.equal("/cosmos.bank.v1beta1.MsgSend");
+                expect(response.txResponse.rawLog).to.contain('fail');
+                expect(response.txResponse.code).to.not.equal(0);
+            })
     )
 
 
@@ -93,14 +128,20 @@ describe('sending transactions', function () {
 
         it("setTransferTaxBp should set transfer tax bp", () =>
             startSwarmWithClient()
-                .then(passThroughAwait(client => setTransferTaxBp(client.bzSdk, BP, {maxGas: MAX_GAS, gasPrice: GAS_PRICE})))
+                .then(passThroughAwait(client => setTransferTaxBp(client.bzSdk, BP, {
+                    maxGas: MAX_GAS,
+                    gasPrice: GAS_PRICE
+                })))
                 .then(client => getTaxInfo(client.bzSdk))
                 .then(taxInfo => expect(taxInfo.transferTaxBp).equal(BP))
         );
 
         it("setTaxCollector should set tax collector", () =>
             startSwarmWithClient()
-                .then(passThroughAwait(client => setTaxCollector(client.bzSdk, TAX_COLLECTOR, {maxGas: MAX_GAS, gasPrice: GAS_PRICE})))
+                .then(passThroughAwait(client => setTaxCollector(client.bzSdk, TAX_COLLECTOR, {
+                    maxGas: MAX_GAS,
+                    gasPrice: GAS_PRICE
+                })))
                 .then(client => getTaxInfo(client.bzSdk))
                 .then(taxInfo => expect(taxInfo.taxCollector).equal(TAX_COLLECTOR))
         );
@@ -121,20 +162,25 @@ describe('sending transactions', function () {
 
         it("setTransferTaxBp should not set transfer tax bp", () =>
             startSwarmWithClient()
-                .then(passThroughAwait(client => setTransferTaxBp(client.bzSdk, BP, {maxGas: MAX_GAS, gasPrice: GAS_PRICE})))
+                .then(passThroughAwait(client => setTransferTaxBp(client.bzSdk, BP, {
+                    maxGas: MAX_GAS,
+                    gasPrice: GAS_PRICE
+                })))
                 .then(client => getTaxInfo(client.bzSdk))
                 .then(taxInfo => expect(taxInfo.transferTaxBp).not.equal(BP))
         );
 
         it("setTaxCollector should not set tax collector", () =>
             startSwarmWithClient()
-                .then(passThroughAwait(client => setTaxCollector(client.bzSdk, TAX_COLLECTOR, {maxGas: MAX_GAS, gasPrice: GAS_PRICE})))
+                .then(passThroughAwait(client => setTaxCollector(client.bzSdk, TAX_COLLECTOR, {
+                    maxGas: MAX_GAS,
+                    gasPrice: GAS_PRICE
+                })))
                 .then(client => getTaxInfo(client.bzSdk))
                 .then(taxInfo => expect(taxInfo.taxCollector).not.equal(TAX_COLLECTOR))
         );
 
     });
-
 
 
 })
