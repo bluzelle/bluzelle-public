e84453b25a1b7726be0f2059ae6e89338ba1d842
smartdev0328 <smartdev0328@gmail.com>
2023-03-02 21:12:39 -0500

    authz-add

--- authz-test/src/authz.js
+++ authz-test/src/authz.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.api = void 0;
 const bluzelle_1 = require("bluzelle");
 const api = (0, bluzelle_1.bluzelle)({
-    mnemonic: 'job outdoor cash total virtual plug grass alone stable club sample sock file toast clock undo diamond jeans sad surface artefact employ wise thing',
+    mnemonic: 'rather expand outer owner happy merit fox endless illegal square faint logic error blur ginger smile myself brush orange alert smile push school divert',
     endpoint: 'http://localhost:1317',
-    uuid: ''
+    uuid: Date.now().toString(),
+    chain_id: 'curium'
 });
 exports.api = api;
--- authz-test/src/authz.ts
+++ authz-test/src/authz.ts
@@ -1,9 +1,10 @@
 import {bluzelle, API} from 'bluzelle';
 
 const api: API = bluzelle({
-    mnemonic: 'job outdoor cash total virtual plug grass alone stable club sample sock file toast clock undo diamond jeans sad surface artefact employ wise thing',
+    mnemonic: 'rather expand outer owner happy merit fox endless illegal square faint logic error blur ginger smile myself brush orange alert smile push school divert',
     endpoint: 'http://localhost:1317',
-    uuid: ''
+    uuid: Date.now().toString(),
+    chain_id: 'curium'
 })
 
 export {api};
