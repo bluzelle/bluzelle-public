403ca2c4533a633fbb60efd07c541635c824a905
smartdev0328 <smartdev0328@gmail.com>
2023-03-09 14:41:29 -0500

    finish

--- authz-test/test/authz.spec.ts
+++ authz-test/test/authz.spec.ts
@@ -31,219 +31,219 @@ describe("Authorization Module Test", function () {
     this.timeout(1_800_000)
     let client: BluzelleClient;
 
-    // it('generic authorization should be successfully created', () => {
-    //     const params: GenericAuthorizationParams = {
-    //         granter: testGranter,
-    //         grantee: testGrantee,
-    //         msg: "/cosmos.gov.v1beta1.MsgSubmitProposal",
-    //         expiration
-    //     }
-    //     return newBluzelleClient({
-    //         wallet,
-    //         url: "http://localhost:26657"
-    //     }).then((client) =>
-    //         genericAuthorizationTx(client, params)
-    //             .then(() => client.queryClient.authz.Grants({
-    //                 granter: testGranter,
-    //                 grantee: testGrantee,
-    //                 msgTypeUrl: "/cosmos.gov.v1beta1.MsgSubmitProposal"
-    //             })
-    //             )
-    //             .then((res) => {
-    //                 expect(res.grants[0].authorization?.typeUrl).to.equal("/cosmos.authz.v1beta1.GenericAuthorization")
-    //                 expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/cosmos.gov.v1beta1.MsgSubmitProposal")
-    //             })
-    //     );
-    // });
+    it('generic authorization should be successfully created', () => {
+        const params: GenericAuthorizationParams = {
+            granter: testGranter,
+            grantee: testGrantee,
+            msg: "/cosmos.gov.v1beta1.MsgSubmitProposal",
+            expiration
+        }
+        return newBluzelleClient({
+            wallet,
+            url: "http://localhost:26657"
+        }).then((client) =>
+            genericAuthorizationTx(client, params)
+                .then(() => client.queryClient.authz.Grants({
+                    granter: testGranter,
+                    grantee: testGrantee,
+                    msgTypeUrl: "/cosmos.gov.v1beta1.MsgSubmitProposal"
+                })
+                )
+                .then((res) => {
+                    expect(res.grants[0].authorization?.typeUrl).to.equal("/cosmos.authz.v1beta1.GenericAuthorization")
+                    expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/cosmos.gov.v1beta1.MsgSubmitProposal")
+                })
+        );
+    });
 
 
-    // it('send authorization should be successfully created', () => {
-    //     const params: SendAuthorizationParams = {
-    //         granter: testGranter,
-    //         grantee: testGrantee,
-    //         spendLimit: [{
-    //             denom: "ubnt",
-    //             amount: "1000000"
-    //         }],
-    //         expiration
-    //     }
-    //     return newBluzelleClient({
-    //         wallet,
-    //         url: "http://localhost:26657"
-    //     }).then((client) =>
-    //         sendAuthorizationTx(client, params)
-    //             .then(() => client.queryClient.authz.Grants({
-    //                 granter: testGranter,
-    //                 grantee: testGrantee,
-    //                 msgTypeUrl: "/cosmos.bank.v1beta1.MsgSend"
-    //             })
-    //             )
-    //             .then((res) => {
-    //                 expect(res.grants[0].authorization?.typeUrl).to.equal("/cosmos.bank.v1beta1.SendAuthorization")
-    //                 expect(SendAuthorization.decode(res.grants[0].authorization?.value as any).spendLimit[0].amount).to.equal('1000000')
-    //             })
-    //     )
-    // });
+    it('send authorization should be successfully created', () => {
+        const params: SendAuthorizationParams = {
+            granter: testGranter,
+            grantee: testGrantee,
+            spendLimit: [{
+                denom: "ubnt",
+                amount: "1000000"
+            }],
+            expiration
+        }
+        return newBluzelleClient({
+            wallet,
+            url: "http://localhost:26657"
+        }).then((client) =>
+            sendAuthorizationTx(client, params)
+                .then(() => client.queryClient.authz.Grants({
+                    granter: testGranter,
+                    grantee: testGrantee,
+                    msgTypeUrl: "/cosmos.bank.v1beta1.MsgSend"
+                })
+                )
+                .then((res) => {
+                    expect(res.grants[0].authorization?.typeUrl).to.equal("/cosmos.bank.v1beta1.SendAuthorization")
+                    expect(SendAuthorization.decode(res.grants[0].authorization?.value as any).spendLimit[0].amount).to.equal('1000000')
+                })
+        )
+    });
 
-    // it(' stake authorization should be successfully created', () => {
-    //     const params: StakeAuthorizationParams = {
-    //         granter: testGranter,
-    //         grantee: testGrantee,
-    //         authorizationType: AuthorizationType.AUTHORIZATION_TYPE_DELEGATE,
-    //         expiration
-    //     }
-    //     return newBluzelleClient({
-    //         wallet,
-    //         url: "http://localhost:26657"
-    //     }).then((client) =>
-    //         stakeAuthorizationTx(client, params)
-    //             .then(() => client.queryClient.authz.Grants({
-    //                 granter: testGranter,
-    //                 grantee: testGrantee,
-    //                 msgTypeUrl: "/cosmos.staking.v1beta1.MsgDelegate"
-    //             })
-    //             )
-    //             .then((res) => {
-    //                 expect(res.grants[0].authorization?.typeUrl).to.equal("/cosmos.staking.v1beta1.StakeAuthorization")
-    //                 expect(StakeAuthorization.decode(res.grants[0].authorization?.value as any).authorizationType).to.equal(AuthorizationType.AUTHORIZATION_TYPE_DELEGATE)
-    //             })
-    //     )
-    // });
+    it(' stake authorization should be successfully created', () => {
+        const params: StakeAuthorizationParams = {
+            granter: testGranter,
+            grantee: testGrantee,
+            authorizationType: AuthorizationType.AUTHORIZATION_TYPE_DELEGATE,
+            expiration
+        }
+        return newBluzelleClient({
+            wallet,
+            url: "http://localhost:26657"
+        }).then((client) =>
+            stakeAuthorizationTx(client, params)
+                .then(() => client.queryClient.authz.Grants({
+                    granter: testGranter,
+                    grantee: testGrantee,
+                    msgTypeUrl: "/cosmos.staking.v1beta1.MsgDelegate"
+                })
+                )
+                .then((res) => {
+                    expect(res.grants[0].authorization?.typeUrl).to.equal("/cosmos.staking.v1beta1.StakeAuthorization")
+                    expect(StakeAuthorization.decode(res.grants[0].authorization?.value as any).authorizationType).to.equal(AuthorizationType.AUTHORIZATION_TYPE_DELEGATE)
+                })
+        )
+    });
 
-    // it(' creat nft authorization should be successfully created', () => {
-    //     const params: GenericAuthorizationParams = {
-    //         granter: testGranter,
-    //         grantee: testGrantee,
-    //         msg: "/bluzelle.curium.nft.MsgCreateNFT",
-    //         expiration
-    //     }
-    //     return newBluzelleClient({
-    //         wallet,
-    //         url: "http://localhost:26657"
-    //     }).then((client) =>
-    //         genericAuthorizationTx(client, params)
-    //             .then(() => client.queryClient.authz.Grants({
-    //                 granter: testGranter,
-    //                 grantee: testGrantee,
-    //                 msgTypeUrl: "/bluzelle.curium.nft.MsgCreateNFT"
-    //             })
-    //             )
-    //             .then((res) => {
-    //                 expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/bluzelle.curium.nft.MsgCreateNFT")
-    //             })
-    //     );
-    // });
+    it(' creat nft authorization should be successfully created', () => {
+        const params: GenericAuthorizationParams = {
+            granter: testGranter,
+            grantee: testGrantee,
+            msg: "/bluzelle.curium.nft.MsgCreateNFT",
+            expiration
+        }
+        return newBluzelleClient({
+            wallet,
+            url: "http://localhost:26657"
+        }).then((client) =>
+            genericAuthorizationTx(client, params)
+                .then(() => client.queryClient.authz.Grants({
+                    granter: testGranter,
+                    grantee: testGrantee,
+                    msgTypeUrl: "/bluzelle.curium.nft.MsgCreateNFT"
+                })
+                )
+                .then((res) => {
+                    expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/bluzelle.curium.nft.MsgCreateNFT")
+                })
+        );
+    });
 
-    // it(' print edition authorization should be successfully created', () => {
-    //     const params: GenericAuthorizationParams = {
-    //         granter: testGranter,
-    //         grantee: testGrantee,
-    //         msg: "/bluzelle.curium.nft.MsgPrintEdition",
-    //         expiration
-    //     }
-    //     return newBluzelleClient({
-    //         wallet,
-    //         url: "http://localhost:26657"
-    //     }).then((client) =>
-    //         genericAuthorizationTx(client, params)
-    //             .then(() => client.queryClient.authz.Grants({
-    //                 granter: testGranter,
-    //                 grantee: testGrantee,
-    //                 msgTypeUrl: "/bluzelle.curium.nft.MsgPrintEdition"
-    //             })
-    //             )
-    //             .then((res) => {
-    //                 expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/bluzelle.curium.nft.MsgPrintEdition")
-    //             })
-    //     );
-    // });
+    it(' print edition authorization should be successfully created', () => {
+        const params: GenericAuthorizationParams = {
+            granter: testGranter,
+            grantee: testGrantee,
+            msg: "/bluzelle.curium.nft.MsgPrintEdition",
+            expiration
+        }
+        return newBluzelleClient({
+            wallet,
+            url: "http://localhost:26657"
+        }).then((client) =>
+            genericAuthorizationTx(client, params)
+                .then(() => client.queryClient.authz.Grants({
+                    granter: testGranter,
+                    grantee: testGrantee,
+                    msgTypeUrl: "/bluzelle.curium.nft.MsgPrintEdition"
+                })
+                )
+                .then((res) => {
+                    expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/bluzelle.curium.nft.MsgPrintEdition")
+                })
+        );
+    });
 
-    // it(' transfer nft authorization should be successfully created', () => {
-    //     const params: GenericAuthorizationParams = {
-    //         granter: testGranter,
-    //         grantee: testGrantee,
-    //         msg: "/bluzelle.curium.nft.MsgTransferNFT",
-    //         expiration
-    //     }
-    //     return newBluzelleClient({
-    //         wallet,
-    //         url: "http://localhost:26657"
-    //     }).then((client) =>
-    //         genericAuthorizationTx(client, params)
-    //             .then(() => client.queryClient.authz.Grants({
-    //                 granter: testGranter,
-    //                 grantee: testGrantee,
-    //                 msgTypeUrl: "/bluzelle.curium.nft.MsgTransferNFT"
-    //             })
-    //             )
-    //             .then((res) => {
-    //                 expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/bluzelle.curium.nft.MsgTransferNFT")
-    //             })
-    //     );
-    // });
+    it(' transfer nft authorization should be successfully created', () => {
+        const params: GenericAuthorizationParams = {
+            granter: testGranter,
+            grantee: testGrantee,
+            msg: "/bluzelle.curium.nft.MsgTransferNFT",
+            expiration
+        }
+        return newBluzelleClient({
+            wallet,
+            url: "http://localhost:26657"
+        }).then((client) =>
+            genericAuthorizationTx(client, params)
+                .then(() => client.queryClient.authz.Grants({
+                    granter: testGranter,
+                    grantee: testGrantee,
+                    msgTypeUrl: "/bluzelle.curium.nft.MsgTransferNFT"
+                })
+                )
+                .then((res) => {
+                    expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/bluzelle.curium.nft.MsgTransferNFT")
+                })
+        );
+    });
 
-    // it(' create collection authorization should be successfully created', () => {
-    //     const params: GenericAuthorizationParams = {
-    //         granter: testGranter,
-    //         grantee: testGrantee,
-    //         msg: "/bluzelle.curium.nft.MsgCreateCollection",
-    //         expiration
-    //     }
-    //     return newBluzelleClient({
-    //         wallet,
-    //         url: "http://localhost:26657"
-    //     }).then((client) =>
-    //         genericAuthorizationTx(client, params)
-    //             .then(() => client.queryClient.authz.Grants({
-    //                 granter: testGranter,
-    //                 grantee: testGrantee,
-    //                 msgTypeUrl: "/bluzelle.curium.nft.MsgCreateCollection"
-    //             })
-    //             )
-    //             .then((res) => {
-    //                 expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/bluzelle.curium.nft.MsgCreateCollection")
-    //             })
-    //     );
-    // });
+    it(' create collection authorization should be successfully created', () => {
+        const params: GenericAuthorizationParams = {
+            granter: testGranter,
+            grantee: testGrantee,
+            msg: "/bluzelle.curium.nft.MsgCreateCollection",
+            expiration
+        }
+        return newBluzelleClient({
+            wallet,
+            url: "http://localhost:26657"
+        }).then((client) =>
+            genericAuthorizationTx(client, params)
+                .then(() => client.queryClient.authz.Grants({
+                    granter: testGranter,
+                    grantee: testGrantee,
+                    msgTypeUrl: "/bluzelle.curium.nft.MsgCreateCollection"
+                })
+                )
+                .then((res) => {
+                    expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/bluzelle.curium.nft.MsgCreateCollection")
+                })
+        );
+    });
 
-    // it('grant should be successfully revoked', async () => {
-    //     const params: RevokeAuthorizationParams = {
-    //         granter: testGranter,
-    //         grantee: testGrantee,
-    //         msgTypeUrl: "/cosmos.gov.v1beta1.MsgSubmitProposal",
-    //     }
-    //     const gParams: GenericAuthorizationParams = {
-    //         granter: testGranter,
-    //         grantee: testGrantee,
-    //         msg: "/cosmos.gov.v1beta1.MsgSubmitProposal",
-    //         expiration
-    //     }
-    //     const client = await newBluzelleClient({
-    //         wallet,
-    //         url: "http://localhost:26657"
-    //     })
-    //     await genericAuthorizationTx(client, gParams);
-    //     const res: any = await client.queryClient.authz.Grants({
-    //         granter: testGranter,
-    //         grantee: testGrantee,
-    //         msgTypeUrl: "/cosmos.gov.v1beta1.MsgSubmitProposal"
-    //     })
-    //     expect(res.grants[0].authorization?.typeUrl).to.equal("/cosmos.authz.v1beta1.GenericAuthorization")
-    //     expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/cosmos.gov.v1beta1.MsgSubmitProposal")
-    //     await revokeAuthorizationTx(client, params);
-    //     const res1 = async () => {
-    //         try {
-    //             await client.queryClient.authz.Grants({
-    //                 granter: testGranter,
-    //                 grantee: testGrantee,
-    //                 msgTypeUrl: "/cosmos.gov.v1beta1.MsgSubmitProposal"
-    //             })
-    //         } catch (e) {
-    //             expect(e).to.match(/NotFound/)
-    //         }
-    //     }
-    //     await res1();
-    // });
+    it('grant should be successfully revoked', async () => {
+        const params: RevokeAuthorizationParams = {
+            granter: testGranter,
+            grantee: testGrantee,
+            msgTypeUrl: "/cosmos.gov.v1beta1.MsgSubmitProposal",
+        }
+        const gParams: GenericAuthorizationParams = {
+            granter: testGranter,
+            grantee: testGrantee,
+            msg: "/cosmos.gov.v1beta1.MsgSubmitProposal",
+            expiration
+        }
+        const client = await newBluzelleClient({
+            wallet,
+            url: "http://localhost:26657"
+        })
+        await genericAuthorizationTx(client, gParams);
+        const res: any = await client.queryClient.authz.Grants({
+            granter: testGranter,
+            grantee: testGrantee,
+            msgTypeUrl: "/cosmos.gov.v1beta1.MsgSubmitProposal"
+        })
+        expect(res.grants[0].authorization?.typeUrl).to.equal("/cosmos.authz.v1beta1.GenericAuthorization")
+        expect(GenericAuthorization.decode(res.grants[0].authorization?.value as any).msg).to.equal("/cosmos.gov.v1beta1.MsgSubmitProposal")
+        await revokeAuthorizationTx(client, params);
+        const res1 = async () => {
+            try {
+                await client.queryClient.authz.Grants({
+                    granter: testGranter,
+                    grantee: testGrantee,
+                    msgTypeUrl: "/cosmos.gov.v1beta1.MsgSubmitProposal"
+                })
+            } catch (e) {
+                expect(e).to.match(/NotFound/)
+            }
+        }
+        await res1();
+    });
 
     it('grant should be successfully executed', async () => {
         const params: ExecuteAuthorizationParams = {
