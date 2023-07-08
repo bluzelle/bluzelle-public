eda41ee76f51875fddfc289340678a22af468eea
smartdev0328 <smartdev0328@gmail.com>
2023-07-08 10:13:41 -0400

    feat: add set-bp, set-tax-collector

--- curium/x/tax/client/cli/tx.go
+++ curium/x/tax/client/cli/tx.go
@@ -2,11 +2,14 @@ package cli
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/bluzelle/bluzelle-public/curium/x/tax/types"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/spf13/cobra"
 )
 
@@ -23,44 +26,51 @@ func GetTxCmd() *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
-	cmd.AddCommand()
+	cmd.AddCommand(GetCmdSetBp())
 	// this line is used by starport scaffolding # 1
 	flags.AddQueryFlagsToCmd(cmd)
 
 	return cmd
 }
 
-// func GetCmdSetBp() *cobra.Command {
-// 	// cmd := &cobra.Command{
-// 	// 	Use:  "set-bp",
-// 	// 	Long: "Set the Tax bp",
-// 	// 	Example: fmt.Sprintf(
-// 	// 		`$ %s tx tax set-bp 200 10`,
-// 	// 		version.AppName,
-// 	// 	),
-// 	// 	RunE: func(cmd *cobra.Command, args []string) error {
-// 	// 		clientCtx, err := client.GetClientTxContext(cmd)
-// 	// 		if err != nil {
-// 	// 			return err
-// 	// 		}
+func GetCmdSetBp() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "set-bp",
+		Short: "Set the Tax bp",
+		Example: fmt.Sprintf(
+			`$ %s tx tax set-bp 200 10`,
+			version.AppName,
+		),
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			bp, err := strconv.Atoi(args[0])
+			creator := clientCtx.GetFromAddress().String()
+			msg := types.NewMsgSetGasTaxBp(creator, int64(bp))
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+	return cmd
+}
 
-// 	// 		data, err := collectNftData(cmd)
-// 	// 		if err != nil {
-// 	// 			return err
-// 	// 		}
-
-// 	// 		msg := types.NewMsgSetGasTaxBp(clientCtx.GetFromAddress())
-
-// 	// 		if err := msg.ValidateBasic(); err != nil {
-// 	// 			return err
-// 	// 		}
-
-// 	// 		return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
-// 	// 	},
-// 	// }
-
-// 	// cmd.Flags().AddFlagSet(FlagCreateNFT())
-// 	// flags.AddTxFlagsToCmd(cmd)
-
-// 	// return cmd
-// }
+func GetCmdSetTaxCollector() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "set-tax-collector",
+		Short: "Set the collector of the tax",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			newCollector := args[0]
+			creator := clientCtx.GetFromAddress().String()
+			msg := types.NewMsgSetTaxCollector(creator, newCollector)
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+	return cmd
+}
