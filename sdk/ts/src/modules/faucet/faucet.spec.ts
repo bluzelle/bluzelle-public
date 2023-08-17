68803c0f5170faad76df0f682dea22ff544aef15
Brady Lee <blee@Bradys-MacBook-Pro.local>
2023-08-17 16:52:38 -0400

    clean up

--- sdk/ts/src/modules/faucet/faucet.spec.ts
+++ sdk/ts/src/modules/faucet/faucet.spec.ts
@@ -1,11 +1,10 @@
-import {defaultSwarmConfig, startSwarmWithClient} from "@bluzelle/testing";
-import {expect} from "chai";
-import {createAddress, mint} from "./tx";
-import {Swarm} from "daemon-manager/src";
-import {BluzelleClient} from "../../core";
-import { getAccountBalance, send } from '../bank';
-import {withCtxAwait} from "@scottburch/with-context";
-import { passThroughAwait } from 'promise-passthrough';
+import { defaultSwarmConfig, startSwarmWithClient } from '@bluzelle/testing';
+import { expect } from 'chai';
+import { createAddress, mint } from './tx';
+import { Swarm } from 'daemon-manager/src';
+import { BluzelleClient } from '../../core';
+import { getAccountBalance } from '../bank';
+import { withCtxAwait } from '@scottburch/with-context';
 
 describe('faucet module', function () {
 
@@ -76,22 +75,5 @@ describe('faucet module', function () {
             .then(info => mint(info.bzSdk))
             .catch(err => expect(err.message.includes('invalid request')).to.be.true)
     );
-
-    it('should not be able to create an account named "minter" then use it to mint tokens if faucet is off', () => {
-      let client: BluzelleClient;
-      return startSwarmWithClient({...defaultSwarmConfig, bluzelleFaucet: false})
-        .then(passThroughAwait(ctx => client = ctx.bzSdk))
-        .then(passThroughAwait(ctx => ctx.swarm.getValidators()[0].exec(`curiumd keys add minter`)))
-        .then(withCtxAwait("keys", ctx => ctx.swarm.getValidators()[0].exec(`curiumd keys list`)))
-        .then(passThroughAwait(ctx => send(client, ctx.keys[0].address, 1000000000, {
-          maxGas: 100000000,
-          gasPrice: 0.002
-        })))
-        .then(({bzSdk}) => client = bzSdk)
-        .then(() => mint(client, 'bluzelle1qst08g0f6hyr7z6a7xpgye3nv4ngtnxzz457zd'))
-        .catch(err => expect(err.stack).to.contain('invalid request'))
-        .then(() => getAccountBalance(client, 'bluzelle1qst08g0f6hyr7z6a7xpgye3nv4ngtnxzz457zd'))
-        .then(bal => expect(bal).to.equal(0))
-    });
 
 });
