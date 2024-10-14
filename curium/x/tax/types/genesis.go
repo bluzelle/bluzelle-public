70ceb7e1d9051ee7f69824fd8acb2b3d2932478b
xbradylee <bjnxlee@gmail.com>
2024-10-14 08:06:28 -0400

    added logs

--- curium/x/tax/types/genesis.go
+++ curium/x/tax/types/genesis.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"fmt"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"strings"
 )
@@ -15,10 +16,12 @@ func DefaultGenesis() *GenesisState {
 func (gs GenesisState) Validate() error {
 	// this line is used by starport scaffolding # genesis/types/validate
 	if gs.GasTaxBp < 0 {
+		fmt.Printf("GAS TAX BP: %d", gs.GasTaxBp)
 		return sdkerrors.New("tax", 1, "Invalid GasTaxBp")
 	}
 
 	if gs.TransferTaxBp < 0 {
+		fmt.Printf("TRANSFER TAX BP: %d", gs.TransferTaxBp)
 		return sdkerrors.New("tax", 1, "Invalid TransferTaxBp")
 	}
 
