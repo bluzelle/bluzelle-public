443206ecb0b56f8c67cc6d04a3d8db4a728f37f3
xbradylee <bjnxlee@gmail.com>
2024-11-14 15:17:06 -0500

    applied fix-storage-pin changes

--- curium/x/storage-ipfs/ipfs/start-node.go
+++ curium/x/storage-ipfs/ipfs/start-node.go
@@ -17,6 +17,7 @@ import (
 	"github.com/bluzelle/ipfs-kubo/repo"
 	"github.com/bluzelle/ipfs-kubo/repo/fsrepo"
 	"github.com/ipfs/go-cid"
+	ipfscid "github.com/ipfs/go-cid"
 
 	// "github.com/ipfs/interface-go-ipfs-core/path"
 	"github.com/ipfs/boxo/path"
@@ -71,9 +72,10 @@ type StorageIpfsNode struct {
 	RepoPath string
 }
 
-func (storageNode *StorageIpfsNode) AddPin(pathString string) error {
-	mPath, _ := path.NewPath(pathString)
-	return storageNode.IpfsApi.Pin().Add(storageNode.Context, mPath)
+func (storageNode *StorageIpfsNode) AddPin(cid string) error {
+	ipfsCid, _ := ipfscid.Decode(cid)
+	ipfsPath := path.FromCid(ipfsCid)
+	return storageNode.IpfsApi.Pin().Add(storageNode.Context, ipfsPath)
 }
 
 func (storageNode *StorageIpfsNode) Stop() error {
