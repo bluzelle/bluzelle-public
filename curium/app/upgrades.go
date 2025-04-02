7fd7cf3dd84689353d52f7021ba79a3e71317a7f
alvi <smartdev0328@gmail.com>
2025-04-03 01:36:37 +0800

    upgrade v11

--- curium/app/upgrades.go
+++ curium/app/upgrades.go
@@ -1,16 +1,19 @@
 package app
 
 import (
-	upgrade "github.com/bluzelle/bluzelle-public/curium/app/upgrades/v10"
+	upgrade "github.com/bluzelle/bluzelle-public/curium/app/upgrades/v11"
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
 func (app *App) setupUpgradeHandlers(
 	configurator module.Configurator,
 ) {
-	app.UpgradeKeeper.SetUpgradeHandler(upgrade.UpgradeName, upgrade.CreateV10UpgradeHandler(app.mm, configurator))
+	baseAppLegacySS := app.ParamsKeeper.Subspace(baseapp.Paramspace).WithKeyTable(paramstypes.ConsensusParamsKeyTable())
+	app.UpgradeKeeper.SetUpgradeHandler(upgrade.UpgradeName, upgrade.CreateV11UpgradeHandler(app.mm, configurator))
 }
 
 func (app *App) setupUpgradeStoreLoaders() {
@@ -21,7 +24,7 @@ func (app *App) setupUpgradeStoreLoaders() {
 	var storeUpgrades *storetypes.StoreUpgrades
 
 	storeUpgrades = &storetypes.StoreUpgrades{
-		Deleted: []string{"faucet"},
+		Added: []string{crisistypes.ModuleName},
 	}
 
 	if storeUpgrades != nil {
