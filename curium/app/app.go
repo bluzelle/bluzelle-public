3ec8cf136fb2ec9db5d1d1ab1c258a3460a06a6e
alvi <smartdev0328@gmail.com>
2025-04-09 02:41:15 +0800

    fix e2e software upgrade test

--- curium/app/app.go
+++ curium/app/app.go
@@ -680,6 +680,7 @@ func NewCuriumApp(
 
 	app.SetAnteHandler(anteHandler)
 	app.SetEndBlocker(app.EndBlocker)
+	app.upgrade(app.Configurator)
 
 	if loadLatest {
 		if err := app.LoadLatestVersion(); err != nil {
