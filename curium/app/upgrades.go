85c247e290d63149370b7e31baaa4b81323a8060
alvi <smartdev0328@gmail.com>
2025-04-11 00:24:04 +0800

    double_supply upgrade handler

--- curium/app/upgrades.go
+++ curium/app/upgrades.go
@@ -2,24 +2,9 @@ package app
 
 import (
 	doublesupply "github.com/bluzelle/bluzelle-public/curium/app/upgrades/doublesupply"
-	nfttypes "github.com/bluzelle/bluzelle-public/curium/x/nft/types"
-	"github.com/cosmos/cosmos-sdk/baseapp"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	consensustypes "github.com/cosmos/cosmos-sdk/x/consensus/types"
-	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
-	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
-	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
-	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
-	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	icahosttypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/host/types"
-	ibctransfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
 )
 
 // NOTE: This upgrade defines a reference implementation of what an upgrade
@@ -29,40 +14,7 @@ import (
 func (app *App) setupUpgradeHandlers(
 	configurator module.Configurator,
 ) {
-	for _, subspace := range app.ParamsKeeper.GetSubspaces() {
-		subspace := subspace
-		var keyTable paramstypes.KeyTable
-		switch subspace.Name() {
-		case authtypes.ModuleName:
-			keyTable = authtypes.ParamKeyTable() //nolint:staticcheck
-		case banktypes.ModuleName:
-			keyTable = banktypes.ParamKeyTable() //nolint:staticcheck
-		case stakingtypes.ModuleName:
-			keyTable = stakingtypes.ParamKeyTable() //nolint:staticcheck
-		case minttypes.ModuleName:
-			keyTable = minttypes.ParamKeyTable() //nolint:staticcheck
-		case distrtypes.ModuleName:
-			keyTable = distrtypes.ParamKeyTable() //nolint:staticcheck
-		case slashingtypes.ModuleName:
-			keyTable = slashingtypes.ParamKeyTable() //nolint:staticcheck
-		case govtypes.ModuleName:
-			keyTable = govv1.ParamKeyTable() //nolint:staticcheck
-		case crisistypes.ModuleName:
-			keyTable = crisistypes.ParamKeyTable() //nolint:staticcheck
-		case nfttypes.ModuleName:
-			keyTable = nfttypes.ParamKeyTable() //nolint:staticcheck
-		case ibctransfertypes.ModuleName:
-			keyTable = ibctransfertypes.ParamKeyTable() //nolint:staticcheck
-		case icahosttypes.SubModuleName:
-			keyTable = icahosttypes.ParamKeyTable() //nolint:staticcheck
-		}
-		if !subspace.HasKeyTable() {
-			subspace.WithKeyTable(keyTable)
-		}
-	}
-	baseAppLegacySS := app.ParamsKeeper.Subspace(baseapp.Paramspace).WithKeyTable(paramstypes.ConsensusParamsKeyTable())
-
-	app.UpgradeKeeper.SetUpgradeHandler(doublesupply.UpgradeName, doublesupply.CreateDoubleSupplyUpgradeHandler(app.mm, configurator, baseAppLegacySS, app.BankKeeper, &app.ConsensusParamsKeeper))
+	app.UpgradeKeeper.SetUpgradeHandler(doublesupply.UpgradeName, doublesupply.CreateDoubleSupplyUpgradeHandler(app.mm, configurator, app.BankKeeper))
 }
 
 func (app *App) setupUpgradeStoreLoaders() {
@@ -71,13 +23,6 @@ func (app *App) setupUpgradeStoreLoaders() {
 		panic("failed to read upgrade info from disk: " + err.Error())
 	}
 	var storeUpgrades *storetypes.StoreUpgrades
-
-	storeUpgrades = &storetypes.StoreUpgrades{
-		Added: []string{
-			crisistypes.ModuleName,
-			consensustypes.ModuleName,
-		},
-	}
 
 	if storeUpgrades != nil {
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, storeUpgrades))
--- curium/app/upgrades/doublesupply/upgrades.go
+++ curium/app/upgrades/doublesupply/upgrades.go
@@ -3,25 +3,19 @@ package v10
 import (
 	"fmt"
 
-	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
-	consensusparamskeeper "github.com/cosmos/cosmos-sdk/x/consensus/keeper"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
-	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
 func CreateDoubleSupplyUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
-	baseAppLegacySS paramstypes.Subspace,
 	bankKeeper bankkeeper.Keeper,
-	consensusParamsKeeper *consensusparamskeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		baseapp.MigrateParams(ctx, baseAppLegacySS, consensusParamsKeeper)
 
 		fmt.Println("UPGRADE HANDLER FOR 'double_supply' HAS BEEN SET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
