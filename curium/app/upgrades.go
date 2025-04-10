7f59fe9d9a3c93224d72ed95f4e27c232dfd3c12
xbradylee <bjnxlee@gmail.com>
2025-04-10 11:07:47 -0400

    do_nothing upgrade handler

--- curium/app/upgrades.go
+++ curium/app/upgrades.go
@@ -1,7 +1,7 @@
 package app
 
 import (
-	upgrade "github.com/bluzelle/bluzelle-public/curium/app/upgrades/v11"
+	donothing "github.com/bluzelle/bluzelle-public/curium/app/upgrades/donothing"
 	nfttypes "github.com/bluzelle/bluzelle-public/curium/x/nft/types"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
@@ -62,7 +62,7 @@ func (app *App) setupUpgradeHandlers(
 	}
 	baseAppLegacySS := app.ParamsKeeper.Subspace(baseapp.Paramspace).WithKeyTable(paramstypes.ConsensusParamsKeyTable())
 
-	app.UpgradeKeeper.SetUpgradeHandler(upgrade.UpgradeName, upgrade.CreateV11UpgradeHandler(app.mm, configurator, baseAppLegacySS, &app.ConsensusParamsKeeper))
+	app.UpgradeKeeper.SetUpgradeHandler(donothing.UpgradeName, donothing.CreateDoNothingUpgradeHandler(app.mm, configurator, baseAppLegacySS, &app.ConsensusParamsKeeper))
 }
 
 func (app *App) setupUpgradeStoreLoaders() {
---
+++ curium/app/upgrades/donothing/upgrades.go
@@ -0,0 +1,25 @@
+package donothing
+
+import (
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	consensusparamskeeper "github.com/cosmos/cosmos-sdk/x/consensus/keeper"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+func CreateDoNothingUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	baseAppLegacySS paramstypes.Subspace,
+	consensusParamsKeeper *consensusparamskeeper.Keeper,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		baseapp.MigrateParams(ctx, baseAppLegacySS, consensusParamsKeeper)
+
+		return mm.RunMigrations(ctx, configurator, fromVM)
+	}
+}
+
+const UpgradeName = "do_nothing"
