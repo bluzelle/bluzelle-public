b2b77e304b04f1be3a344e557905e91b7e81ca45
xbradylee <bjnxlee@gmail.com>
2025-04-25 13:37:05 -0400

    updated tax admin addr

--- curium/app/types/global/global.go
+++ curium/app/types/global/global.go
@@ -2,5 +2,5 @@ package global
 
 const (
 	Denom        = "ubnt"
-	AdminAddress = "bluzelle1dvc2u4l84hyfeem5fmfm9eyjlndpsycwwfhtln"
+	AdminAddress = "bluzelle1t95s6zzf58y6nsny9uhdap6ej7ddkga9c4htd6"
 )
