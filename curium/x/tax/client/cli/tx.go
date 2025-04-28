be9b37940b0e912ab33c96cae595a17dc7dc3eee
Alvi <smartdev0328@gmail.com>
2025-04-29 02:01:47 +0800

    fix: tax tx cmds

--- curium/x/tax/client/cli/tx.go
+++ curium/x/tax/client/cli/tx.go
@@ -42,7 +42,7 @@ func GetCmdSetGasTaxBp() *cobra.Command {
 		Use:  "set-gas-tax-bp",
 		Long: "Set gas tax basis point",
 		Example: fmt.Sprintf(
-			`$ %s tx tax set-bp 200 10`,
+			`$ %s tx tax set-gas-tax-bp 10`,
 			version.AppName,
 		),
 		Args: cobra.ExactArgs(1),
@@ -51,7 +51,7 @@ func GetCmdSetGasTaxBp() *cobra.Command {
 			if err != nil {
 				return err
 			}
-			bp, err := strconv.Atoi(args[0])
+			bp, _ := strconv.Atoi(args[0])
 			creator := clientCtx.GetFromAddress().String()
 			msg := types.NewMsgSetGasTaxBp(creator, int64(bp))
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
@@ -64,6 +64,10 @@ func GetCmdSetTaxCollector() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:  "set-tax-collector",
 		Long: "Set tax collector",
+		Example: fmt.Sprintf(
+			`$ %s tx tax set-tax-collector bluzelle1pcx0ant83addkeaskrm7c9wkamwnj3hh2qc9j7`,
+			version.AppName,
+		),
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			clientCtx, err := client.GetClientQueryContext(cmd)
@@ -83,13 +87,17 @@ func GetCmdSetTransferTaxBp() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:  "set-transfer-tx-bp",
 		Long: "Set transfer tax basis point",
+		Example: fmt.Sprintf(
+			`$ %s tx tax set-transfer-tax-bp 10`,
+			version.AppName,
+		),
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
 				return err
 			}
-			bp, err := strconv.Atoi(args[0])
+			bp, _ := strconv.Atoi(args[0])
 			creator := clientCtx.GetFromAddress().String()
 			msg := types.NewMsgSetTransferTaxBp(creator, int64(bp))
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
