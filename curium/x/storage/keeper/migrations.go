aaaf8ae9a354d9ce76938701b5c21a89abb05d65
alvi <alvi@bluzelle.com>
2025-07-09 20:34:17 +0300

    fix: storage module migration

---
+++ curium/x/storage/keeper/migrations.go
@@ -0,0 +1,19 @@
+package keeper
+
+import (
+	v2 "github.com/bluzelle/bluzelle-public/curium/x/storage/migrations/v2"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type Migrator struct {
+	keeper Keeper
+}
+
+func NewMigrator(keeper Keeper) Migrator {
+	return Migrator{keeper: keeper}
+}
+
+func (m Migrator) Migrate1to2(ctx sdk.Context) error {
+
+	return v2.MigrateStore(ctx, m.keeper.StoreKey, m.keeper.Cdc)
+}
