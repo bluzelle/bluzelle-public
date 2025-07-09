14845f8f4601149f701435d5a09e60719bac5e58
alvi <smartdev0328@gmail.com>
2025-07-09 20:37:53 +0300

    migration-test

---
+++ curium/x/storage/migrations/v2/migrations.go
@@ -0,0 +1,31 @@
+package v11 // migrations.go
+
+import (
+	v1 "github.com/bluzelle/bluzelle-public/curium/x/storage/migrations/v1/types"
+	"github.com/bluzelle/bluzelle-public/curium/x/storage/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
+	store := ctx.KVStore(storeKey)
+	oldStoreIter := store.Iterator(nil, nil)
+
+	for ; oldStoreIter.Valid(); oldStoreIter.Next() {
+		key := oldStoreIter.Key()
+		oldVal := store.Get(key)
+		oldPin := &v1.MsgPin{}
+		cdc.MustUnmarshal(oldVal, oldPin)
+		newPin := &types.MsgPin{
+			Creator: oldPin.Creator,
+			Cid:     oldPin.Cid,
+			Addrs:   nil,
+		}
+		store.Delete(key)
+		pin := cdc.MustMarshal(newPin)
+		store.Set(key, pin)
+	}
+
+	return nil
+}
