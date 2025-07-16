d9e56d51444b7b80737101bc5f5bd9f6d0b32937
alvi <alvi@bluzelle.com>
2025-07-16 19:33:22 +0300

    fix: sdk storage test

--- sdk/ts/src/modules/storage/storage.spec.ts
+++ sdk/ts/src/modules/storage/storage.spec.ts
@@ -1,186 +1,156 @@
-import {getBlzClient, restartIpfsServerAndSwarm} from "@bluzelle/testing/src/commonUtils";
 import {generateContent} from "@bluzelle/testing/src/fileUtils";
 import {BehaviorSubject, of} from "rxjs";
-import {times} from "lodash";
 import {passThroughAwait} from "promise-passthrough";
-import {createCtx, withCtxAwait} from "@scottburch/with-context";
+import { withCtxAwait} from "@scottburch/with-context";
 import {expect} from "chai";
 import delay from "delay";
-import {defaultSwarmConfig, stopIpfsServers} from "@bluzelle/testing";
+import {defaultSwarmConfig} from "@bluzelle/testing";
 import {hasContent} from "./query";
-import {getAccountBalance} from "../bank";
 import {pinCid} from "./tx";
-import {getTx, withTransaction} from "../../core";
-import { stopSwarm } from "@bluzelle/testing/src/swarmUtils";
-import {create} from "ipfs-http-client"
+import {getTx} from "../../core";
+import { startSwarmWithClient, stopSwarm } from "@bluzelle/testing/src/swarmUtils";
+import axios from 'axios'
+import FormData from 'form-data'
 
 const curiumUrl = 'http://localhost:26667';
 const mnemonic = new BehaviorSubject<string>("");
 
+async function loadCID() {
+    const { CID } = await eval('import("multiformats/cid")');
+    return CID;
+}
 
-const ipfs = create({
-  url: 'http://172.26.13.123:5001'  // Your Go IPFS node's endpoint
-})
 
-async function uploadToIPFS() {
-  const content = 'Hello IPFS from Node 22!'
-  const { cid } = await ipfs.add(content)
-  console.log('✅ CID:', cid.toString())
+async function uploadToIpfs(contentObj) {
+  const form = new FormData()
+  
+  // Example 1: Upload a file
+  form.append('file', contentObj,  {
+        filename: 'upload.txt',
+        contentType: 'application/octet-stream',
+    })
+
+  // OR Example 2: Upload raw buffer
+  // const buffer = Buffer.from('Hello from Axios + IPFS!')
+  // form.append('file', buffer, 'hello.txt')
+
+  const res = await axios.post('http://54.227.217.212:5001/api/v0/add', form, {
+    headers: form.getHeaders()
+  })
+  console.log('✅ Uploaded CID:', res.data.Hash)
+  return {
+    path: res.data.Hash
+  }
+
 }
 
 describe('storage module', function () {
     this.timeout(600_000);
 
-    // beforeEach(() =>
-    //     restartIpfsServerAndSwarm(({...defaultSwarmConfig}))
-    //         .then((swarmMnemonic) => mnemonic.next(swarmMnemonic))
-    // );
-    
-    // after(() =>
-    //     stopSwarm({...defaultSwarmConfig})
-    //     .then(stopIpfsServers)
-    // );
+    after(() =>
+        stopSwarm({...defaultSwarmConfig})
+    );
 
     it('hasContent should return true if content is pinned', () =>
-        
-            Promise.resolve(uploadToIPFS())
-            .then((addResults) => console.log(addResults))
-            .catch(e => console.log(e))
+        startSwarmWithClient({
+          config: defaultSwarmConfig
+        })
+        .then(withCtxAwait('addResult', ()=> uploadToIpfs(generateContent(0.01))))
+        .then(passThroughAwait(()=> delay(20_000)))
+        .then(passThroughAwait((ctx) =>
+            pinCid(ctx.bzSdk, { cid: ctx.addResult.path }, { maxGas: 200000, gasPrice: 0.002, mode: 'sync' })
+            // .then(() => console.log)
+          )
+        )
+        .then(passThroughAwait(()=> delay(20_000)))
+        .then(ctx => 
+            hasContent(ctx.bzSdk, ctx.addResult.path)
+        )
+        .then((confirm) => expect(confirm).to.be.true)
+      )
+
+    it('hasContent should return false if content is NOT pinned', () =>
+        startSwarmWithClient({
+          config: defaultSwarmConfig
+        })
+        .then(withCtxAwait('addResult', ()=> uploadToIpfs(generateContent(0.01))))
+        .then(passThroughAwait(()=> delay(20_000)))
+        .then(ctx => 
+            hasContent(ctx.bzSdk, ctx.addResult.path)
+        )
+        .then((confirm) => expect(confirm).to.be.false)
     );
-    // it('hasContent should return true if content is pinned', () =>
-    //     Promise.all(times(2).map(() =>
-    //         Promise.resolve(generateContent(0.01))
-    //             .then((content) => createHeliaClient().then((heliaClient) => heliaClient.add(content)))
-    //     ))
-    //         .then((addResults) => createCtx('addResults', () => addResults))
-    //         .then(withCtxAwait('client', () => getBlzClient(curiumUrl, mnemonic.getValue())))
-    //         .then(passThroughAwait((ctx) =>
-    //           ctx.addResults.map((addResult: any) =>
-    //             pinCid(ctx.client, { cid: addResult.path }, { maxGas: 200000, gasPrice: 0.002, mode: 'sync' })
-    //           )
-    //         ))
-    //         .then(ctx => ctx.addResults.map((addResult: any) =>
-    //             hasContent(ctx.client, addResult.path)
-    //                 .then((confirm) => expect(confirm).to.be.true)
-    //         ))
-    // );
 
 
-    // it('hasContent should return false if content is NOT pinned', () =>
-    //     Promise.all(times(2).map(() =>
-    //         Promise.resolve(generateContent(0.01))
-    //             .then((content) => createHeliaClient().then((heliaClient) => heliaClient.add(content)))
-    //     ))
-    //         .then((addResults) => createCtx('addResults', () => addResults))
-    //         .then(withCtxAwait('client', () => getBlzClient(curiumUrl, mnemonic.getValue())))
-    //         .then(ctx => Promise.all(ctx.addResults.map((addResult: any) =>
-    //             hasContent(ctx.client, addResult.path)
-    //                 .then((confirm) => expect(confirm).to.be.false)
-    //         )))
-    // );
+    it('should query for v1 cids', () =>
+        startSwarmWithClient({
+          config: defaultSwarmConfig
+        })
+        .then(passThroughAwait(ctx =>
+                pinCid(ctx.bzSdk, {cid: 'bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi'}, {
+                    maxGas: 10000000,
+                    gasPrice: 0.002,
+                    mode: 'sync'
+                })
+        ))
+        .then(passThroughAwait(() => delay(6_000)))
+        .then(ctx => hasContent(ctx.bzSdk, 'bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi'))
+        .then(resp => expect(resp).to.be.true)
+    );
 
 
-    // it('getAccountBalance should return account balance', () =>
-    //     Promise.all(times(5).map(() =>
-    //         Promise.resolve({content: generateContent(0.01)})
-    //             .then(ctx => createHeliaClient().then((heliaClient) => heliaClient.add(ctx.content)
-    //                 .then((addResult: any) => ({content: ctx.content, cid: addResult.path}))))
-    //     ))
-    //         .then((contents) => createCtx('contents', () => contents))
-    //         .then(withCtxAwait('client', () => getBlzClient(curiumUrl, mnemonic.getValue())))
-    //         .then(passThroughAwait(ctx => withTransaction(ctx.client, () =>
-    //             ctx.contents.forEach((contentObj: any) => pinCid(ctx.client, {cid: contentObj.cid}, {
-    //                 maxGas: 200000,
-    //                 gasPrice: 0.002,
-    //                 mode: 'sync'
-    //             }))
-    //         )))
-    //         .then(ctx => getAccountBalance(ctx.client, ctx.client.address))
-    // );
+    it('should query for the same cid with either v0 or v1', () =>
+        startSwarmWithClient({
+          config: defaultSwarmConfig
+        })
+        .then(passThroughAwait(ctx =>
+                pinCid(ctx.bzSdk, {cid: 'bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi'}, {
+                    maxGas: 10000000,
+                    gasPrice: 0.002,
+                    mode: 'sync'
+                })
+        ))
+        .then(passThroughAwait(() => delay(6_000)))
+        .then(ctx => hasContent(ctx.bzSdk, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR'))
+        .then(resp => expect(resp).to.be.true)
+    );
 
 
-    // it('should query for v1 cids', () =>
-    //     getBlzClient(curiumUrl, mnemonic.getValue())
-    //         .then(passThroughAwait(bzSdk =>
-    //             pinCid(bzSdk, {cid: 'bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi'}, {
-    //                 maxGas: 10000000,
-    //                 gasPrice: 0.002,
-    //                 mode: 'sync'
-    //             })
-    //         ))
-    //         .then(passThroughAwait(() => delay(6_000)))
-    //         .then(bzSdk => hasContent(bzSdk, 'bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi'))
-    //         .then(resp => expect(resp).to.be.true)
-    // );
+    it('should query for the same cid with either v0 or v1 other direction', () =>
+        startSwarmWithClient({
+          config: defaultSwarmConfig
+        })
+        .then(passThroughAwait(ctx =>
+                pinCid(ctx.bzSdk, {cid: 'bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi'}, {
+                    maxGas: 10000000,
+                    gasPrice: 0.002,
+                    mode: 'sync'
+                })
+        ))
+            .then(passThroughAwait(() => delay(12_000)))
+            .then(ctx => loadCID().then(CID => 
+                hasContent(ctx.bzSdk, CID.parse('bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi').toV0().toString())
+            ))
+            .then(resp => expect(resp).to.be.true)
+    );
 
 
-    // it('should query for the same cid with either v0 or v1', () =>
-    //     getBlzClient(curiumUrl, mnemonic.getValue())
-    //         .then(passThroughAwait(bzSdk =>
-    //             loadCID().then(CID => 
-    //                 pinCid(bzSdk, {cid: CID.parse('bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi').toV0().toString()}, {
-    //                     maxGas: 10000000,
-    //                     gasPrice: 0.002,
-    //                     mode: 'sync'
-    //                 })
-    //             )
-    //         ))
-    //         .then(passThroughAwait(() => delay(6_000)))
-    //         .then(bzSdk => hasContent(bzSdk, 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR'))
-    //         .then(resp => expect(resp).to.be.true)
-    // );
-
-
-    // it('should query for the same cid with either v0 or v1 other direction', () =>
-    //     getBlzClient(curiumUrl, mnemonic.getValue())
-    //         .then(passThroughAwait(bzSdk =>
-    //             (pinCid(bzSdk, {cid: 'bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi'}, {
-    //                 maxGas: 10000000,
-    //                 gasPrice: 0.002,
-    //                 mode: 'sync'
-    //             }) as any)
-    //                 .then(console.log)
-    //         ))
-    //         .then(passThroughAwait(() => delay(12_000)))
-    //         .then(bzSdk => loadCID().then(CID => 
-    //             hasContent(bzSdk, CID.parse('bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi').toV0().toString())
-    //         ))
-    //         .then(resp => expect(resp).to.be.true)
-    // );
-
-
-    // it('should query a transaction by hash', () => {
-    //     return getBlzClient(curiumUrl, mnemonic.getValue())
-    //         .then(bzSdk =>
-    //             (pinCid(bzSdk, {cid: 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR'}, {
-    //                 maxGas: 10000000,
-    //                 gasPrice: 0.002,
-    //                 mode: 'sync'
-    //             }) as any)
-    //                 .then((resp: any) => ({
-    //                     resp,
-    //                     bzSdk
-    //                 }))
-    //         )
-    //         .then(({resp, bzSdk}) => getTx(bzSdk, resp.transactionHash))
-    // });
-
-    // it('should allow multiaddr to nodes holding content', () => {
-    //     const addr = '/ip4/45.32.211.194/udp/4001/quic-v1/p2p/12D3KooWDpD6gVGLAr7UZGUUgzVfXA6C3wotrBzY9CT8hQFhrKA7/p2p-circuit/p2p/12D3KooWM7UBZn1hS96xXMvE2UthmXTMmXq3w1p4r14MiSZqkaMa'
-    //     return getBlzClient(curiumUrl, mnemonic.getValue())
-    //         .then(bzSdk => (pinCid(bzSdk, {cid: 'QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR', addresses: ['/ip4/45.32.211.194/udp/4001/quic-v1/p2p/12D3KooWDpD6gVGLAr7UZGUUgzVfXA6C3wotrBzY9CT8hQFhrKA7/p2p-circuit/p2p/12D3KooWM7UBZn1hS96xXMvE2UthmXTMmXq3w1p4r14MiSZqkaMa']}, {
-    //                 maxGas: 10000000,
-    //                 gasPrice: 0.002,
-    //                 mode: 'sync'
-    //             }) as any)
-    //                 .then((resp: any) => ({
-    //                     resp,
-    //                     bzSdk
-    //                 }))
-    //         )
-    //        // .then(({resp, bzSdk}) => hasContent(bzSdk, ))
-    //       .catch(err => console.log(`pinCid did not work. Error: ${err}`));
-
-    // })
+    it('should query a transaction by hash', () => {
+      return startSwarmWithClient({
+                config: defaultSwarmConfig
+              })
+              .then(ctx =>
+                      (pinCid(ctx.bzSdk, {cid: 'bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi'}, {
+                          maxGas: 10000000,
+                          gasPrice: 0.002,
+                          mode: 'sync'
+                      }) as any)
+                      .then((resp: any) => ({
+                        resp,
+                        bzSdk: ctx.bzSdk
+                      }))
+              )
+            .then(({resp, bzSdk}) => getTx(bzSdk, resp.transactionHash))
+    });
 
 });
