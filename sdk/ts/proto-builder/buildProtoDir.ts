2de9fd9e7123822d97e44c63c518786678504784
alvi <alvi@bluzelle.com>
2025-07-16 20:15:29 +0300

    fix build-sdk

--- sdk/ts/proto-builder/buildProtoDir.ts
+++ sdk/ts/proto-builder/buildProtoDir.ts
@@ -3,7 +3,10 @@ import fetch from 'node-fetch'
 import {getCosmosProtoDependencies, getGoogleDependencies, getThirdPartyDependencies} from "./getProto";
 import {readdirSync, statSync} from "fs";
 import * as path from 'path'
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const rootDir = () => path.join(__dirname, '../../..');
 const getCuriumProtoDir = () => path.join(rootDir(), 'curium/proto');
