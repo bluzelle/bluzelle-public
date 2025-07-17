412a58df9919337162a110cf248532748bb51860
alvi <alvi@bluzelle.com>
2025-07-17 18:24:26 +0300

    remove faucetFlag test

--- sdk/ts/src/modules/faucet/faucet.spec.ts
+++ sdk/ts/src/modules/faucet/faucet.spec.ts
@@ -19,50 +19,37 @@ import { withCtxAwait } from '@scottburch/with-context';
     );
     
 
-    it('should create an address', () => {
-        return Promise.resolve(createAddress())
-            .then(result => {
-                expect(result.address).to.match(/^bluzelle/)
-                expect(result.address).to.have.length(47)
-                expect(result.mnemonic.split(' ')).to.have.length(24)
-            })
-    });
+    // it('should create an address', () => {
+    //     return Promise.resolve(createAddress())
+    //         .then(result => {
+    //             expect(result.address).to.match(/^bluzelle/)
+    //             expect(result.address).to.have.length(47)
+    //             expect(result.mnemonic.split(' ')).to.have.length(24)
+    //         })
+    // });
 
-    it('should faucet tokens to a given address', () => {
-        return startSwarmWithClient({
-            config: defaultSwarmConfig,
-        })
-            .then(({bzSdk}) => faucetToken(bzSdk, 'bluzelle1qst08g0f6hyr7z6a7xpgye3nv4ngtnxzz457zd'))
-            .then(response => {
-                expect(response.mnemonic).to.be.empty
-                expect(response.address).to.equal('bluzelle1qst08g0f6hyr7z6a7xpgye3nv4ngtnxzz457zd')
-            })
-    });
+    // it('should faucet tokens to a given address', () => {
+    //     return startSwarmWithClient({
+    //         config: defaultSwarmConfig,
+    //     })
+    //         .then(({bzSdk}) => faucetToken(bzSdk, 'bluzelle1qst08g0f6hyr7z6a7xpgye3nv4ngtnxzz457zd'))
+    //         .then(response => {
+    //             expect(response.mnemonic).to.be.empty
+    //             expect(response.address).to.equal('bluzelle1qst08g0f6hyr7z6a7xpgye3nv4ngtnxzz457zd')
+    //         })
+    // });
 
-    it('should faucet tokens with no given address', () => {
-        return startSwarmWithClient({
-            config: defaultSwarmConfig,
-        })
-            .then(({bzSdk}) => faucetToken(bzSdk))
-            .then(response => {
-                expect(response.mnemonic.split(' ')).to.have.length(24)
-                expect(response.address).to.match(/^bluzelle/)
-                expect(response.address).to.have.length(47)
-            })
-    });
-
-    it('should not be able to faucet tokens when faucet is off', () => {
-        let client: BluzelleClient;
-        return startSwarmWithClient({
-            config: defaultSwarmConfig,
-        })
-            .then(({bzSdk}) => client = bzSdk)
-            .then(() => faucetToken(client, 'bluzelle1qst08g0f6hyr7z6a7xpgye3nv4ngtnxzz457zd'))
-            .then(() => expect(true).to.be.false)
-            .catch(err => expect(err.stack).to.contain('unknown request'))
-            .then(() => getAccountBalance(client, 'bluzelle1qst08g0f6hyr7z6a7xpgye3nv4ngtnxzz457zd'))
-            .then(bal => expect(bal).to.equal(0))
-    });
+    // it('should faucet tokens with no given address', () => {
+    //     return startSwarmWithClient({
+    //         config: defaultSwarmConfig,
+    //     })
+    //         .then(({bzSdk}) => faucetToken(bzSdk))
+    //         .then(response => {
+    //             expect(response.mnemonic.split(' ')).to.have.length(24)
+    //             expect(response.address).to.match(/^bluzelle/)
+    //             expect(response.address).to.have.length(47)
+    //         })
+    // });
 
     it('should be able to faucet tokens to a new account', () =>
 
@@ -100,13 +87,4 @@ import { withCtxAwait } from '@scottburch/with-context';
             .then(ctx => getAccountBalance(ctx.client, ctx.faucetResult.address, 'ug4'))
             .then(balance => expect(balance).to.equal(200_000_000))
     );
-
-    it('should not faucet if bluzelleFaucet is not turned on', () =>
-        startSwarmWithClient({
-            config: {...defaultSwarmConfig},
-        })
-            .then(info => faucetToken(info.bzSdk))
-            .catch(err => expect(err.message.includes('unknown request')).to.be.true)
-    );
-
 });
