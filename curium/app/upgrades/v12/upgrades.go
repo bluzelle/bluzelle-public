d659553cc3b94468400bc11a0b19b36eaf313157
xbradylee <bjnxlee@gmail.com>
2025-08-27 14:50:20 -0400

    added v12.0 upgrade

---
+++ curium/app/upgrades/v12/upgrades.go
@@ -0,0 +1,25 @@
+package v12
+
+import (
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	consensusparamskeeper "github.com/cosmos/cosmos-sdk/x/consensus/keeper"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+func CreateV12UpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	baseAppLegacySS paramstypes.Subspace,
+	consensusParamsKeeper *consensusparamskeeper.Keeper,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		baseapp.MigrateParams(ctx, baseAppLegacySS, consensusParamsKeeper)
+
+		return mm.RunMigrations(ctx, configurator, fromVM)
+	}
+}
+
+const UpgradeName = "v12.0"
