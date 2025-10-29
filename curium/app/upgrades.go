c2e2736c4fecac53d509a2ad517dda80e1b69ef5
alvi <alvi@bluzelle.com>
2025-10-29 21:07:07 +0200

    upgrade handler

--- curium/app/upgrades.go
+++ curium/app/upgrades.go
@@ -3,6 +3,11 @@ package app
 import (
 	upgrade "github.com/bluzelle/bluzelle-public/curium/app/upgrades/v12"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	ibctransfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
+	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
+	connectiontypes "github.com/cosmos/ibc-go/v8/modules/core/03-connection/types"
+	ibcexported "github.com/cosmos/ibc-go/v8/modules/core/exported"
 )
 
 // NOTE: This upgrade defines a reference implementation of what an upgrade
@@ -12,51 +17,45 @@ import (
 func (app *App) setupUpgradeHandlers(
 	configurator module.Configurator,
 ) {
-	// for _, subspace := range app.ParamsKeeper.GetSubspaces() {
-	// 	subspace := subspace
-	// 	var keyTable paramstypes.KeyTable
-	// 	switch subspace.Name() {
-	// 	case authtypes.ModuleName:
-	// 		keyTable = authtypes.ParamKeyTable() //nolint:staticcheck
-	// 	case banktypes.ModuleName:
-	// 		keyTable = banktypes.ParamKeyTable() //nolint:staticcheck
-	// 	case stakingtypes.ModuleName:
-	// 		keyTable = stakingtypes.ParamKeyTable() //nolint:staticcheck
-	// 	case minttypes.ModuleName:
-	// 		keyTable = minttypes.ParamKeyTable() //nolint:staticcheck
-	// 	case distrtypes.ModuleName:
-	// 		keyTable = distrtypes.ParamKeyTable() //nolint:staticcheck
-	// 	case slashingtypes.ModuleName:
-	// 		keyTable = slashingtypes.ParamKeyTable() //nolint:staticcheck
-	// 	case govtypes.ModuleName:
-	// 		keyTable = govv1.ParamKeyTable() //nolint:staticcheck
-	// 	case crisistypes.ModuleName:
-	// 		keyTable = crisistypes.ParamKeyTable() //nolint:staticcheck
-	// 	case nfttypes.ModuleName:
-	// 		keyTable = nfttypes.ParamKeyTable() //nolint:staticcheck
-	// 	case ibctransfertypes.ModuleName:
-	// 		keyTable = ibctransfertypes.ParamKeyTable() //nolint:staticcheck
-	// 	case icahosttypes.SubModuleName:
-	// 		keyTable = icahosttypes.ParamKeyTable() //nolint:staticcheck
-	// 	}
-	// 	if !subspace.HasKeyTable() {
-	// 		subspace.WithKeyTable(keyTable)
-	// 	}
-	// }
+	// Fix IBC module parameters before migration
+	// This is required for v0.47 to v0.50 upgrade
+
+	// Fix IBC core module parameters
+	ibcSubspace, exists := app.ParamsKeeper.GetSubspace(ibcexported.ModuleName)
+	if exists && !ibcSubspace.HasKeyTable() {
+		// Create parameter key table for IBC core module with required parameters
+		keyTable := paramstypes.NewKeyTable()
+
+		// Register client parameters (includes AllowedClients)
+		keyTable.RegisterParamSet(&clienttypes.Params{})
+
+		// Register connection parameters (includes MaxExpectedTimePerBlock)
+		keyTable.RegisterParamSet(&connectiontypes.Params{})
+
+		ibcSubspace.WithKeyTable(keyTable)
+	}
+
+	// Fix IBC transfer module parameters
+	transferSubspace, exists := app.ParamsKeeper.GetSubspace(ibctransfertypes.ModuleName)
+	if exists && !transferSubspace.HasKeyTable() {
+		// Create parameter key table for IBC transfer module
+		keyTable := paramstypes.NewKeyTable()
+
+		// Register transfer parameters (includes SendEnabled)
+		keyTable.RegisterParamSet(&ibctransfertypes.Params{})
+
+		transferSubspace.WithKeyTable(keyTable)
+	}
 
 	app.UpgradeKeeper.SetUpgradeHandler(upgrade.UpgradeName, upgrade.CreateV12UpgradeHandler(app.mm, configurator))
 }
 
-// func (app *App) setupUpgradeStoreLoaders() {
-// 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
-// 	if err != nil {
-// 		panic("failed to read upgrade info from disk: " + err.Error())
-// 	}
-// 	storeUpgrades := &storetypes.StoreUpgrades{}
-// 	app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, storeUpgrades))
-// }
+func (app *App) setupUpgradeStoreLoaders() {
+	// No store upgrades needed for v0.47 to v0.50 migration
+	// Crisis and consensus stores already exist from previous upgrades
+}
 
 func (app *App) upgrade(configurator module.Configurator) {
 	app.setupUpgradeHandlers(configurator)
-	// app.setupUpgradeStoreLoaders()
+	app.setupUpgradeStoreLoaders()
 }
