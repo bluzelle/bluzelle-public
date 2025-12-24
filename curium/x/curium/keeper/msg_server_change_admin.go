473257cb564c70a433d4bf1540a94a6a62e141b8
alvi <alvi@bluzelle.com>
2025-12-24 18:53:11 +0200

    change admin

---
+++ curium/x/curium/keeper/msg_server_change_admin.go
@@ -0,0 +1,32 @@
+package keeper
+
+import (
+	"context"
+
+	"cosmossdk.io/errors"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/bluzelle/bluzelle-public/curium/x/curium/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k msgServer) ChangeAdmin(goCtx context.Context, msg *types.MsgChangeAdmin) (*types.MsgChangeAdminResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Get the current admin address
+	currentAdmin := k.GetAdminAddress(ctx)
+	if currentAdmin == "" {
+		return nil, errors.Wrap(sdkerrors.ErrInvalidRequest, "admin address not set")
+	}
+
+	// Verify that the creator is the current admin
+	if msg.Creator != currentAdmin {
+		return nil, errors.Wrap(sdkerrors.ErrUnauthorized, "only the current admin can change the admin address")
+	}
+
+	// Set the new admin address
+	k.SetAdminAddress(ctx, msg.NewAdmin)
+
+	return &types.MsgChangeAdminResponse{}, nil
+}
+
